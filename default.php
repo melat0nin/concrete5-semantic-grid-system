e859e1400297bf9d4eb1854d2562e12f0432d38c
melat0nin <laurence.diver@gmail.com>
2013-03-11 13:24:06 +0000

    better page type starting points

--- default.php
+++ default.php
@@ -1,5 +1,13 @@
 <?php
-
 	defined('C5_EXECUTE') or die("Access Denied."); 
+        $this->inc('elements/header.php');
+?>
 
+
+
+
+
+
+<?php
+    $this->inc('elements/footer.php');
 ?>
--- normal.php
+++ normal.php
@@ -1,23 +1,3 @@
 <?php
-
-	defined('C5_EXECUTE') or die("Access Denied."); 
-
-	$this->inc("elements/header.php");
-	
-?>
-
-	<!-- Main Body -->
-	<article id="maincolumn">
-		<h2>Main</h2>
-	</article>
-
-	<!-- Sidebar -->
-	<section id="sidebar">
-		<h2>Sidebar</h2>
-	</section>
-	
-<?php
-
-	$this->inc("elements/footer.php");
-	
-?>
+    defined('C5_EXECUTE') or die("Access Denied."); 
+    $this->inc("default.php");
